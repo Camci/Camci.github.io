19de90c2ed6e6dc648c13c7fd4f7b8a46b03caa0
Mustafa <66529060+Camci@users.noreply.github.com>
2024-07-19 17:23:12 -0400

    Add files via upload

--- invoice.js
+++ invoice.js
@@ -196,10 +196,11 @@ function ready(fn) {
             const tokenInfo = await grist.docApi.getAccessToken({readOnly: true});
             const img = document.getElementById('the_image');
       
+            console.log("Item has an Img array: ", item.Img);
             // Debugging: Log the attachment ID and tokenInfo
-            const id = `${item.Img.tableId}[[${item.Img.rowIds[0]}]]`;
+            //const id = `${item.Img.tableId}[[${item.Img.rowIds[0]}]]`;
                                     // in a cell, for this example we just take the first.
-            console.log("Attachment id: ", id);
+            //console.log("Attachment id: ", id);
             console.log("tokenInfo: ", tokenInfo);
       
             const src = `${tokenInfo.baseUrl}/attachments/${id}/download?auth=${tokenInfo.token}`;
