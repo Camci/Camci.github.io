98ee9bd7e7ceedb5d63f792ee31bf661e06298b3
Mustafa <66529060+Camci@users.noreply.github.com>
2024-07-24 17:19:57 -0400

    Add files via upload

--- invoice.js
+++ invoice.js
@@ -261,11 +261,6 @@ function ready(fn) {
             imageLoadStatus[index] = false;
             console.log(`Failed to load image ${index} on retry, set placeholder.`);
           };
-        // Check if the Description contains 'Earring' or 'Ring' and set the style
-        if (item.Description && (item.Description.includes('Earring') || item.Description.includes('Ring'))) {
-          console.log("HERE IS THE DESCRIPTION", item.Description);
-          img.style.height = '110px';
-        }
         }
       }
     });
