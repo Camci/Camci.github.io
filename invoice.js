99d94852b1f257e6d0d537a95deb3ee2a0f95600
Mustafa <66529060+Camci@users.noreply.github.com>
2024-07-26 17:05:16 -0400

    Add files via upload

--- invoice.js
+++ invoice.js
@@ -356,6 +356,8 @@ function ready(fn) {
         wireAccountList() {
           return this.invoice.Wire_Account ? this.invoice.Wire_Account.split(',') : [];
         },
+      },
+      methods: {
         optionsList(item) {
           return item.options ? item.options.split(',') : [];
         }
